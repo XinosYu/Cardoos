1d4125378a25f37e6e4455df584eb4d73aadf867
XinosYu <76880814+XinosYu@users.noreply.github.com>
2025-08-02 04:54:05 +0800

    Add files via upload

---
+++ cards/cards.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var velocity = Vector2.ZERO
+var damping = 0.35
+var stiffness = 500
+
+enum cardState{following, dragging}
+@export var cardCurrentState=cardState.following
+
+@export var follow_target:Node
+
+func _process(delta: float) -> void:
+	match cardCurrentState:
+		cardState.dragging:
+			var target_position=get_global_position()-size/2
+			global_position=global_position.lerp(target_position,0.4)
+		cardState.following:
+			if follow_target!=null:
+				var target_position=follow_target.global_position
+				var displacement=target_position-global_position
+				var force=displacement*stiffness
+				velocity += force * delta
+				velocity*=(1.0-damping)
+				global_position+=velocity*delta
+
+func _on_button_button_down() -> void:
+	cardCurrentState = cardState.dragging
+	pass # Replace with function body.
+	
+func _on_button_button_up() -> void:
+	cardCurrentState = cardState.following
+	pass # Replace with function body.
+	
+	
